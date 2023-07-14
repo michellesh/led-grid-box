53f9fc5d94badd6b6213451f1c9e72aa9ce14f6b
Michelle Sharer <michellesharer@gmail.com>
2023-07-14 10:12:24 -0600

    Switch back previous strategy with delay adjusted by average loop time

--- led-grid-box/led-grid-box.ino
+++ led-grid-box/led-grid-box.ino
@@ -1,7 +1,6 @@
 #include "Numbers.h"
 #include <FastLED.h>
 #include <LEDMatrix.h>
-#include <TimeLib.h>
 
 #define BUTTON_PIN 2
 #define LED_PIN 3
@@ -15,17 +14,17 @@
 cLEDMatrix<WIDTH, HEIGHT, HORIZONTAL_ZIGZAG_MATRIX> leds;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(500);
 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds[0], NUM_LEDS);
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-
-  setTime(18, 24, 0, 8, 11, 2023);
 }
 
 void loop() {
+  unsigned long startMillis = millis();
+
   FastLED.clear();
 
   // Test read button state
@@ -36,8 +35,25 @@ void loop() {
     // Serial.println("OFF");
   }
 
-  int hour = hourFormat12();
-  showTime(hour / 10, hour % 10, minute() / 10, minute() % 10);
+  static long hour = 10;
+  static long minute = 9;
+  static long second = 0;
+
+  second++;
+  if (second > 59) {
+    second = 0;
+    minute++;
+    if (minute > 59) {
+      minute = 0;
+      hour++;
+      if (hour > 23) {
+        hour = 0;
+      }
+    }
+  }
+
+  int h = convert24To12Hour(hour);
+  showTime(h / 10, h % 10, minute / 10, minute % 10);
 
   // flipHorizontal();
   flipVertical();
@@ -45,7 +61,28 @@ void loop() {
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.show();
 
-  delay(1000);
+  delay(1000 - getAvgLoopTime(startMillis));
+}
+
+int getAvgLoopTime(unsigned long startMillis) {
+  static unsigned long sum = 0;
+  static unsigned long count = 0;
+  unsigned long endMillis = millis();
+
+  count++;
+  sum += endMillis - startMillis;
+  float avgLoopTime = (float)sum / (float)count;
+  Serial.println(endMillis - startMillis);
+  Serial.println(avgLoopTime);
+  Serial.println(round(avgLoopTime));
+  Serial.println();
+
+  return round(avgLoopTime);
+}
+
+int convert24To12Hour(int hour24) {
+  int hour12 = hour24 > 12 ? hour24 - 12 : hour24;
+  return hour12 == 0 ? 12 : hour12;
 }
 
 void showTime(int d1, int d2, int d3, int d4) {
