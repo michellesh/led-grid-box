04dfd6a1c62f71d49fc78ca036441e4e85191565
Michelle Sharer <michellesharer@gmail.com>
2023-08-02 21:51:31 -0600

    Add challenge solution

---
+++ examples/challenge-solution.ino
@@ -0,0 +1,11 @@
+// Left LEDs
+leds(0, 0) = CRGB::Red;
+leds(0, 4) = CRGB::Red;
+
+// Middle LEDs
+leds(7, 2) = CRGB::Red;
+leds(8, 2) = CRGB::Red;
+
+// Right LEDs
+leds(15, 0) = CRGB::Red;
+leds(15, 4) = CRGB::Red;
